e8ce7f9457439321d1aa0124d2c29f4e7404c42f
SimonBerens <sberensnyc@gmail.com>
2019-02-24 16:39:43 -0500

    Calibration

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from eye_recognition.eye_recognizer import EyeRecognizer, RANGE_X, RANGE_Y
 import cv2
 import pyautogui
 
-CAMERA = 0
+CAMERA = 1
+pyautogui.FAILSAFE = False
 
 if __name__ == '__main__':
     print("Starting...")
@@ -25,10 +26,20 @@ Y88b 888 888       Y88b 888 Y8b.
             """)
 
     size = pyautogui.size()
-    pastx = [size[0] // 2] * 10
-    pasty = [size[1] // 2] * 10
-    past_click = [False] * 10
+    pastx = [0] * 10
+    pasty = [0] * 10
+    blinks = [False] * 10
 
+    calibrated = False
+    lxc = []
+    rxc = []
+    lyc = []
+    ryc = []
+
+    lxca = 0
+    rxca = 0
+    lyca = 0
+    ryca = 0
     while True:
         # escape to exit
         if cv2.waitKey(int(1000 / 100)) == 27:
@@ -46,19 +57,42 @@ Y88b 888 888       Y88b 888 Y8b.
             ly = info["left_eye"]["position"][1]
             rx = info["right_eye"]["position"][0]
             ry = info["right_eye"]["position"][1]
-        elif not info["left_eye"]["status"]:
-            blink = True
+        elif not info["left_eye"]["status"] and not info["right_eye"]["status"]:
+            lx = rx = size[0] // 2
+            ly = ry = size[1] // 2
+        elif info["left_eye"]["status"]:
+            rx = lx = info["left_eye"]["position"][0]
+            ry = ly = info["left_eye"]["position"][1]
         else:
-            continue
+            blink = True
+            lx = rx = info["right_eye"]["position"][0]
+            ly = ry = info["right_eye"]["position"][1]
 
         # moving average of 10 frames
-        pastx.insert(0, size[0] * ((lx + rx) // 2 + RANGE_X // 2) // RANGE_X)
+        pastx.insert(0, (lx - lxca + rx - rxca) // 2)
         del pastx[-1]
-        pasty.insert(0, size[1] * ((ly + ry) // 2 + RANGE_Y // 2) // RANGE_Y)
+        pasty.insert(0, (ly - lyca + ry - ryca) // -2)
         del pasty[-1]
-        past_click.insert(0, blink)
-        del past_click[-1]
-        pyautogui.moveTo(sum(pastx) // len(pastx), sum(pasty) // len(pasty))
+        blinks.insert(0, blink)
+        del blinks[-1]
+
+        if not calibrated:
+            print("Please look at the middle of the screen")
+            if len(lyc) == 50:
+                lxca = sum(lxc) // len(lxc)
+                rxca = sum(rxc) // len(rxc)
+                lyca = sum(lyc) // len(lyc)
+                ryca = sum(ryc) // len(ryc)
+                print(lyca, ryca)
+                calibrated = True
+            else:
+                lxc.append(lx)
+                rxc.append(rx)
+                lyc.append(ly)
+                ryc.append(ry)
+        else:
+            (pyautogui.drag if blinks.count(True) > 5 else pyautogui.move)(sum(pastx) // len(pastx),
+                                                                           sum(pasty) // len(pasty))
 
     cap.release()
     cv2.destroyAllWindows()
