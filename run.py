86f6d2ca57f9d53f45065f029077921f899516e9
SimonBerens <sberensnyc@gmail.com>
2019-02-24 11:38:56 -0500

    Add mouse control

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 from eye_recognition.eye_recognizer import EyeRecognizer
 import cv2
+import pyautogui
 
-CAMERA = 0
+
+CAMERA = 1
 
 if __name__ == '__main__':
+    print("Starting...")
+
     cap = cv2.VideoCapture(CAMERA)
     recoginzer = EyeRecognizer(cap)
 
@@ -19,16 +23,39 @@ Y88b 888 888       Y88b 888 Y8b.
                         888               
                    Y8b d88P               
                     "Y88P" 
+                    
+                    
+                    
+
             """)
 
     while True:
 
-
         info = recoginzer.get_new_info()  # simon work with this :)
 
-        print(info)
+        # eyes not detected
+        if not info["status"]:
+            continue
 
-        if cv2.waitKey(int(1000/30)) == 27:
+        # left click
+        if not info["left_eye"]["status"]:
+            continue
+
+        # right click
+        if not info["right_eye"]["status"]:
+            continue
+
+        lx = info["left_eye"]["position"][0]
+        ly = info["left_eye"]["position"][1]
+        rx = info["right_eye"]["position"][0]
+        ry = info["right_eye"]["position"][1]
+
+        size = pyautogui.size()
+        pyautogui.moveTo(size[0] * ((lx + rx) // 2 + 100) // 200, size[1] * ((ly + ry) // 2 + 100) // 200)
+
+
+        # escape to exit
+        if cv2.waitKey(int(1000 / 30)) == 27:
             break
 
     cap.release()
