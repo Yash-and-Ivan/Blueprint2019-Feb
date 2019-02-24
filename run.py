6e0c0e79242e7ee39d4701ecdd7ec0f06ab3b2c7
igalakhov <igalakhov00@stuy.edu>
2019-02-24 11:08:23 -0500

    added logo

--- run.py
+++ run.py
@@ -7,7 +7,22 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(CAMERA)
     recoginzer = EyeRecognizer(cap)
 
+    print("""
+         8888888888                       
+         888                              
+         888                              
+888  888 8888888   888  888  .d88b.       
+888  888 888       888  888 d8P  Y8b      
+888  888 888       888  888 88888888      
+Y88b 888 888       Y88b 888 Y8b.          
+ "Y88888 8888888888 "Y88888  "Y8888       
+                        888               
+                   Y8b d88P               
+                    "Y88P" 
+            """)
+
     while True:
+
 
         info = recoginzer.get_new_info()  # simon work with this :)
 
