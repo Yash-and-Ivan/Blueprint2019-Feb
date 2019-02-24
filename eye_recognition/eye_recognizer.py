540cccae6b4c2736e6c704a6b9425e8589e0c7b8
igalakhov <igalakhov00@stuy.edu>
2019-02-24 07:52:06 -0500

    more work

---
+++ eye_recognition/eye_recognizer.py
@@ -0,0 +1,6 @@
+
+
+class EyeRecognizer():
+
+    def __init__(self):
+        print("WASSUP")
---
+++ run.py
@@ -0,0 +1,4 @@
+from eye_recognition.eye_recognizer import EyeRecognizer
+
+if __name__ == '__main__':
+    recoginzer = EyeRecognizer()
